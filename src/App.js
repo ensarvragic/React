edaaa0bfde6fb8f83585b44233482e941d287033
ensarvragic <vragicensar@gmail.com>
2023-04-18 17:16:44 +0200

    rendered cards

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
+import React from 'react';
 import './App.css';
 import Header from './components/header/header';
 import Main from './components/main/main';
-import Footer from './components/footer/footer'
-import Card from './components/Card/card'
+import Footer from './components/footer/footer';
+import Card from './components/Card/card';
+
+const cardData = [
+  {
+    id: 1,
+    title: 'Dubai',
+    about: 'Dubai je najgusci i najnaseljeniji grad u Ujedinjenim emiratima',
+    price: '799$',
+  },
+  {
+    id: 2,
+    title: 'Burj Khalifa',
+    about: 'Burj Khalifa je neboder u dubai-u i najveca zgrada na svetu',
+    price: '45.21$',
+  },
+  {
+    id: 3,
+    title: 'Waterfall',
+    about: 'Dubai waterfalls se nalaze na vestackom jezeru burj khalifa od 12 hektara',
+    price: 'free to see',
+  },
+];
 
 
 function App() {
   return (
-  <>
     <div className='App'>
       <Header/>
       <div className="card-container">
-        <Card
-        title = 'Dubai'
-        about = 'Dubai je najgusci i najnaseljeniji grad u Ujedinjenim emiratima'
-        price = '799$'
-        />
-        <Card
-         title = 'Burj Khalifa'
-         about = 'Burj Khalifa je neboder u dubai-u i najveca zgrada na svetu'
-         price = '45.21$'
-        />
-        <Card
-        title = 'Waterfall'
-        about = 'Dubai waterfalls se nalaze na vestackom jezeru burj khalifa od 12 hektara'
-        price = 'free to see'
-        />
+      {cardData.map((card) => (
+      <Card
+        key={card.id}
+        title={card.title}
+        about={card.about}
+        price={card.price}
+      />
+     ))}
       </div>
     </div>
-  </>
   );
 }
 
--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -2,17 +2,17 @@ import React from 'react'
 import './card.css'
 import Image from '../images/BurjK.jpg'
 
-const Card = (props) => {
+const Card = ({title, about, price}) => {
     return(
         <div className="card">
                 <img alt="" src={Image} />
         <div className='text-area'>
-            <h1>{props.title}</h1>
-            <span>{props.about}</span>
+            <h1>{title}</h1>
+            <span>{about}</span>
         </div>
         <div className="programming">
-            <p>{props.title} </p>
-            <p>- {props.price}</p>
+            <p>{title}</p>
+            <p>- {price}</p>
         </div>
         </div>
     )
